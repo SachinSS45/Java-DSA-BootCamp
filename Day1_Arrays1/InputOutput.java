7e982b4e52a911f5a28135efca1e4084dcf3dfec
Sachin Shetkar <ssshetkar2001@gmail.com>
2024-01-11 19:50:36 +0530

    Arrays

---
+++ Day1_Arrays1/InputOutput.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class InputOutput{
+
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+
+		//Creating array
+		int[] arr = new int[5];
+
+		System.out.println("Enter the elements of Array : ");
+		
+		//Talking input from User
+		for(int i = 0; i< arr.length; i++){
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println();//for new Line 
+		//Printing the element by using for loop
+		
+		System.out.println("Elements in array : ");
+		for(int i=0; i<arr.length; i++){
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
---
+++ Day1_Arrays1/Program1.java
@@ -0,0 +1,28 @@
+class public Program1{
+
+	public static void main(String[] args){
+		
+		//Basic Syntax of Array
+		//DataType[] arrayName = new DataType[size];
+
+		int[] arr = new int[6];
+
+		//intialization
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		arr[5] = 60;
+
+		//Printing the element of array
+		System.out.println(arr[0]);//10
+
+		//Updating value of array
+		arr[0] = 70;
+		System.out.println(arr[0]);//70
+		
+
+	}
+}
+
